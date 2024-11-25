889057c8d474cf365ece2669e18ed31e8e3b66cd
Deepak Singh <deepak.businessforweb@gmail.com>
2024-11-25 11:31:09 +0530

    fixed some bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -107,7 +107,7 @@ const loginUser = asyncHandler(async (req, res) => {
   // send token as cookie
 
   const { email, username, password } = req.body;
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, "username or email is required");
   }
 
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import {asyncHandler} from '../utils/asyncHandler'
-import {ApiError} from '../utils/ApiError'
+import {asyncHandler} from '../utils/asyncHandler.js'
+import {ApiError} from '../utils/ApiError.js'
 import jwt from 'jsonwebtoken'
-import {User} from '../models/user.model'
+import {User} from '../models/user.model.js'
 
 export const verifyJWT = asyncHandler(async(req,_,next)=> {
  try {
@@ -12,7 +12,7 @@ export const verifyJWT = asyncHandler(async(req,_,next)=> {
     }
    
    
-       const decodedToken = jwt.verify(token, proccess.env.ACCESS_TOKEN_SECRET)
+       const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
    
       const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
        if (!user) {
